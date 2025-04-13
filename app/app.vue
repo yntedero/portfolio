f9da5a359e130fba60cc5344c7a787c0350b05de
Yurii Ostapchuk <yntedero@gmail.com>
2025-04-13 12:18:18 +0200

    feature: Add navigation links and about page template

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,14 @@
 <template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
   <div>
     <NuxtPage />
   </div>
---
+++ app/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This page is about me and myself
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
